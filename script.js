4223adfdad6647d46a6c440cc7811f9d0c90def5
rajiss-ctrl <omosanjos77@gmail.com>
2020-03-27 14:35:50 -0700

    javatask commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = 'rajiss';
+var courses = ['html','css','js','php'];
+console.log(name,courses);
+var n =courses.length;
+console.log(n);
+if (n % 2 == 0){
+    for(let i = 0; i < 200; i++){
+        {
+            console.log(courses,i);
+        }
+    }
+
+}
